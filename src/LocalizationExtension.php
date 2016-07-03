2e1e719dab925aa82e9d1af4ca2be5fa272c47cf
Dusan Kmet <morlokdk@gmail.com>
2016-07-03 08:09:24 +0200

    fixed di container

--- src/LocalizationExtension.php
+++ src/LocalizationExtension.php
@@ -10,17 +10,25 @@ class LocalizationExtension extends CompilerExtension
 	public $defaults = array(
 		'translatesDir' => null,
 		'debugMode' => null,
+		'sections' => null,
 		'alias' => null,
-		'sections' => null
 	);
+
+	private $tempDir;
+
+
+	public function __construct($tempDir = null)
+	{
+		$this->tempDir = $tempDir;
+	}
 
 
 	public function loadConfiguration()
 	{
 		$container = $this->getContainerBuilder();
 		$config = $this->getConfig($this->defaults);
-
-		$debugMode = $config['debugMode'] === null ? $container->parameters['debugMode'] : $config['debugMode'];
+		$debugMode = $config['debugMode'] !== null ? $config['debugMode'] : $container->parameters['debugMode'];
+		$tempDir = $this->tempDir ? $this->tempDir : $container->parameters['tempDir'];
 
 		$container->addDefinition($this->prefix('translatesStorage'))
 			->setClass('Smartsupp\Localization\DirectoryStorage')
@@ -29,20 +37,19 @@ class LocalizationExtension extends CompilerExtension
 		$container->addDefinition($this->prefix('translatesLoader'))
 			->setClass('Smartsupp\Localization\TranslatesLoader')
 			->addSetup('$debugMode', array($debugMode))
-			->addSetup('setTempDir', array('%tempDir%/cache/_Wimo.Localization'));
+			->addSetup('setTempDir', array($tempDir . '/cache/_Wimo.Localization'));
 
 		$translatorFactory = $container->addDefinition($this->prefix('translatorFactory'))
 			->setClass('Smartsupp\Localization\TranslatorFactory')
 			->addSetup('$debugMode', array($debugMode));
 
+		if ($config['sections']) {
+			$translatorFactory->addSetup('$defaultSections', array($config['sections']));
+		}
 		if ($config['alias']) {
 			foreach ($config['alias'] as $from => $to) {
 				$translatorFactory->addSetup('setAlias', array($from, $to));
 			}
-		}
-
-		if ($config['sections']) {
-			$translatorFactory->addSetup('$defaultSections', array($config['sections']));
 		}
 	}
 
